e2127e4c847fdbd801e2f8147304be1a48d1ef81
SerAbinU1 <abinyt1234@gmail.com>
2025-12-25 07:00:16 +0000

    Add script to ease nvim updates

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -7,64 +7,41 @@ return {
 		end,
 	},
 	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+		},
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
 
-			-- Setup for C++
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
+			-- List of servers to ensure are installed
+			local servers = { "clangd", "ts_ls", "pyright", "jdtls", "gopls" }
 
-			-- Setup for JavaScript/TypeScript
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+			-- CONFIGURATION
+			-- Register configs using vim.lsp.config (Neovim 0.11+)
 
-			-- Setup for Python
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-
-			-- Java (Eclipse JDT LS)
-			lspconfig.jdtls.setup({
-				capabilities = capabilities,
-			})
-
-			-- Setup for Go
-			lspconfig.gopls.setup({
+			-- 1. Configure gopls with custom settings
+			vim.lsp.config("gopls", {
 				capabilities = capabilities,
 				settings = {
 					gopls = {
-						completeUnimported = true, -- Auto-import packages
-						usePlaceholders = true, -- Add placeholders for function parameters
+						completeUnimported = true,
+						usePlaceholders = true,
 						analyses = {
 							unusedparams = true,
-							shadow = true, -- Warn when you accidentally shadow a variable (very useful)
-							nilness = true, -- Warn about useless nil checks
-							unusedwrite = true, -- Warn about writes to variables that are never read,
+							shadow = true,
+							nilness = true,
+							unusedwrite = true,
 						},
-
-						-- Enable "codelens" (actionable text appearing above code)
 						codelenses = {
-							gc_details = false, -- Show where the compiler makes optimization choices
-							generate = true, -- Show a "run" link for //go:generate directives
+							gc_details = false,
+							generate = true,
 							regenerate_cgo = true,
 							run_govulncheck = true,
-							test = true, -- Show "Run test" / "Debug test" above test functions
-							tidy = true, -- Show "Run go mod tidy" if go.mod is dirty
+							test = true,
+							tidy = true,
 						},
-
-						-- Enable inlay hints (ghost text showing variable types/names)
 						hints = {
 							assignVariableTypes = true,
 							compositeLiteralFields = true,
@@ -73,18 +50,34 @@ return {
 							parameterNames = true,
 							rangeVariableTypes = true,
 						},
-
-						-- Enable staticcheck (an advanced linter) directly in gopls
 						staticcheck = true,
 					},
 				},
 			})
 
-			-- Common key mappings
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			-- 2. Configure other servers with default settings + capabilities
+			for _, server in ipairs(servers) do
+				if server ~= "gopls" then
+					vim.lsp.config(server, {
+						capabilities = capabilities,
+					})
+				end
+			end
+
+			-- SETUP MASON-LSPCONFIG
+			require("mason-lspconfig").setup({
+				ensure_installed = servers,
+				automatic_installation = true,
+			})
+			
+			-- Enable servers explicitly? 
+			-- mason-lspconfig v2's 'automatic_enable' (if active) will enable them.
+			-- If not, we can iterate and enable. But usually vim.lsp.config is sufficient for auto-start on filetype?
+			-- Actually, standard vim.lsp.start logic relies on filetype autocomamnds.
+			-- If we want to be sure:
+			for _, server in ipairs(servers) do
+				vim.lsp.enable(server)
+			end
 		end,
 	},
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,50 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
 		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects", -- add this
+			{
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				lazy = true, -- prevent immediate loading which crashes if treesitter isn't ready
+			},
 		},
 		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
+			-- Require configs
+			local configs = require("nvim-treesitter.configs")
+
+			-- Setup treesitter
+			configs.setup({
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
-
-				-- add this block
+				-- We can define textobjects config here, but we need to ensure the plugin is loaded first/during.
+				-- However, nvim-treesitter will try to load modules 'textobjects.selection' etc from the RTP.
 				textobjects = {
 					select = {
 						enable = true,
-						lookahead = true, -- automatically jump forward to textobject
+						lookahead = true,
 						keymaps = {
-							["af"] = "@function.outer", -- around entire function
-							["if"] = "@function.inner", -- inside function body only
-							["ac"] = "@class.outer", -- optional: select around class
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
 							["ic"] = "@class.inner",
 						},
 					},
 				},
 			})
+			-- Explicitly setup hooks if needed, but usually configs.setup handles it IF the plugin is in path.
+			-- With lazy=true above, we might need to manually load it?
+			-- Actually, dependencies are usually loaded when the plugin loads.
+			-- If we make it lazy=true, lazy.nvim might NOT add it to RTP until requested?
+			-- Wait, `dependencies` in lazy.nvim: "Plugins that should be installed/loaded when this plugin is loaded".
+			-- So it will load textobjects when treesitter loads.
+			-- The crash happens because textobjects plugin runs `require('nvim-treesitter.configs')`.
+			-- If treesitter setup hasn't run yet, but treesitter module IS in lua path, it should work.
+			-- The error `module 'nvim-treesitter.configs' not found` implies it is NOT in lua path.
+			-- This implies nvim-treesitter is NOT added to RTP yet when textobjects tries to run.
+			
+			-- Alternative Fix: Remove dependency from here and make it a separate spec that depends on treesitter.
 		end,
 	},
 }
