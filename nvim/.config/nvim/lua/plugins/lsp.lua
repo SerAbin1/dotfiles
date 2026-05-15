91f6b3be0df94ae3edb61fcafef6003805ffe7e2
SerAbinU1 <abinyt1234@gmail.com>
2026-05-15 20:47:27 +0530

    fix: remove unneccesary lsps

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,83 +2,82 @@ require("mason").setup()
 
 -- LSP keymaps (attached per buffer)
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        local buf = args.buf
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local buf = args.buf
 
-        local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = buf, desc = desc })
-        end
+    local map = function(keys, func, desc)
+      vim.keymap.set("n", keys, func, { buffer = buf, desc = desc })
+    end
 
-        -- Info
-        if client.server_capabilities.hoverProvider then
-            map("K", vim.lsp.buf.hover, "Hover")
-        end
+    -- Info
+    if client.server_capabilities.hoverProvider then
+      map("K", vim.lsp.buf.hover, "Hover")
+    end
 
-        map("<leader>sh", vim.lsp.buf.signature_help, "Signature help")
+    map("<leader>sh", vim.lsp.buf.signature_help, "Signature help")
 
-        -- Refactoring
-        map("<leader>rn", vim.lsp.buf.rename, "Rename")
-        map("<leader>ca", vim.lsp.buf.code_action, "Code action")
-
-      end,
+    -- Refactoring
+    map("<leader>rn", vim.lsp.buf.rename, "Rename")
+    map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+  end,
 })
 
 vim.lsp.inlay_hint.enable(true)
 
 -- Format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    callback = function()
-        vim.lsp.buf.format({ async = false })
-    end,
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
 })
 
 -- local servers = { "clangd", "ts_ls", "pyright", "jdtls", "gopls" }
 
-local servers = { "clangd", "pyright", "dartls", "rust_analyzer" }
+local servers = { "dartls", "rust_analyzer", "vtsls", "lua_ls" }
 
 vim.lsp.config("gopls", {
-    settings = {
-        gopls = {
-            completeUnimported = true,
-            usePlaceholders = true,
-            analyses = {
-                unusedparams = true,
-                shadow = true,
-                nilness = true,
-                unusedwrite = true,
-            },
-            codelenses = {
-                gc_details = false,
-                generate = true,
-                regenerate_cgo = true,
-                run_govulncheck = true,
-                test = true,
-                tidy = true,
-            },
-            hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-            },
-            staticcheck = true,
-        },
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+        nilness = true,
+        unusedwrite = true,
+      },
+      codelenses = {
+        gc_details = false,
+        generate = true,
+        regenerate_cgo = true,
+        run_govulncheck = true,
+        test = true,
+        tidy = true,
+      },
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+      staticcheck = true,
     },
+  },
 })
 
 for _, server in ipairs(servers) do
-    if server ~= "gopls" and server ~= "dartls" then
-        vim.lsp.config(server, {})
-    end
+  if server ~= "gopls" and server ~= "dartls" then
+    vim.lsp.config(server, {})
+  end
 end
 
 vim.lsp.config("dartls", {
-    cmd = { vim.fn.expand("~/dev/fvm/default/bin/dart"), "language-server", "--protocol=lsp" },
+  cmd = { vim.fn.expand("~/fvm/default/bin/dart"), "language-server", "--protocol=lsp" },
 })
 
 for _, server in ipairs(servers) do
-    vim.lsp.enable(server)
+  vim.lsp.enable(server)
 end
